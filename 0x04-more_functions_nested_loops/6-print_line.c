6e281d921ff29e371459a08117506e778501a98b
Gemachu Mitiku <99259978+Gsoft2022@users.noreply.github.com>
2022-03-17 14:53:48 -0700

    Add files via upload

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - function that checks for uppercase character.
+ * @n: is the integer for the paramaters of my function
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+_putchar('_');
+_putchar('\n');
+}
